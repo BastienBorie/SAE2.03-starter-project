5de29e46706ad0086b29f54585dd108ce510ccfc
Borie Bastien <borie54@mmi-dev.unilim.fr>
2025-04-11 11:58:48 +0200

    ok je crois voir le bout du tunel

--- admin/data/dataMovie.js
+++ admin/data/dataMovie.js
@@ -17,44 +17,22 @@ let DataMovie = {};
  */
 
 DataMovie.add = async function (fdata) {
-    // console.log("DataMovie.add 1"); // Point de repère n°1
-    let config = {
-      method: "POST", // méthode HTTP à utiliser
-      body: fdata, // données à envoyer sous forme d'objet FormData
-    };
-    // console.log("DataMovie.add 2 ", config); // Point de repère n°2
-  
-    let answer = await fetch(
-      HOST_URL + "/server/script.php?todo=updateMovie",
-      config
-    );
-    // console.log("DataMovie.add 3 "); // Point de repère n°3
-    let data = await answer.json();
-    return data;
+  // console.log("DataMovie.add 1"); // Point de repère n°1
+  let config = {
+    method: "POST", // méthode HTTP à utiliser
+    body: fdata, // données à envoyer sous forme d'objet FormData
   };
+  // console.log("DataMovie.add 2 ", config); // Point de repère n°2
 
- /**
-     * Fetches data from the server based on the specified day.
-     *
-     * @param {string} day - The day parameter to be sent to the server.
-     * @returns The response from the server.
-     * 
-     * DataMovie.request permet de récupérer des données depuis le serveur.
-     * Elle prend en paramètre un jour (lundi mardi...) de la semaine et 
-     * renvoie les données contenues dans la réponse du serveur (data).
-     */
-DataMovie.request = async function(day){
-    // fetch permet d'envoyer une requête HTTP à l'URL spécifiée. 
-    // L'URL est construite en concaténant HOST_URL à "/server/script.php?direction=" et la valeur de la variable dir. 
-    // L'URL finale dépend de la valeur de HOST_URL et de dir.
-    let answer = await fetch(HOST_URL + "/server/script.php?jour=" + day);
-    // answer est la réponse du serveur à la requête fetch.
-    // On utilise ensuite la méthode json() pour extraire de cette réponse les données au format JSON.
-    // Ces données (data) sont automatiquement converties en objet JavaScript.
-    let data = await answer.json();
-    // Enfin, on retourne ces données.
-    return data;
-}
+  let answer = await fetch(
+    HOST_URL + "/server/script.php?todo=updateMovie",
+    config
+  );
+  // console.log("DataMovie.add 3 "); // Point de repère n°3
+  let data = await answer.json();
+  return data;
+};
+
 
 /** DataMovie.update
  * 
@@ -69,7 +47,7 @@ DataMovie.request = async function(day){
  * @param {*} fdata un objet FormData contenant les données du formulaire à envoyer au serveur.
  * @returns la réponse du serveur.
  */
-DataMovie.update = async function (fdata) {
+DataMovie.add = async function (fdata) {
     // fetch possède un deuxième paramètre (optionnel) qui est un objet de configuration de la requête HTTP:
     //  - method : la méthode HTTP à utiliser (GET, POST...)
     //  - body : les données à envoyer au serveur (sous forme d'objet FormData ou bien d'une chaîne de caractères, par exempe JSON)
@@ -77,7 +55,8 @@ DataMovie.update = async function (fdata) {
         method: "POST", // méthode HTTP à utiliser
         body: fdata // données à envoyer sous forme d'objet FormData
     };
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=update", config);
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=updateMovie", config);
+    console.log(answer);
     let data = await answer.json();
     return data;
 }
